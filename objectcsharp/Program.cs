5c20988e01c3771a2a5bf14c945bb2f6e1133d1d
unknown <Administrator@ICTINTERN-PC.Billserver.kiwasco.co.ke>
2023-06-14 15:08:55 +0300

    party system

---
+++ objectcsharp/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        vip vipp = new vip();
+        normal normp = new normal();
+        int person;
+
+        Console.WriteLine("Enter the number of persons: ");
+        person = int.Parse(Console.ReadLine());
+
+        if (person > 100 && person <= 500)
+        {
+            vipp.info();
+        }
+        else
+        {
+            normp.info();
+        }
+    }
+}
+public class details
+{
+    public int id;
+    public string fullname;
+    public int chirno;
+}
+
+public class vip : details
+{
+    public int vtno;
+    public void info()
+    {
+        
+        Console.WriteLine("welcome big man you are most valuable");
+    }
+}
+public class normal : details
+{
+    public int ntno;
+    public void info()
+    {
+        Console.WriteLine("welcome here a gain");
+    }
+}
